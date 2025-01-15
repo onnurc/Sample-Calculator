cb7db474dfccedf5bcc934544ba8417a91b4ef29
Onur çetinkaya <onurcetinkaya149@gmail.com>
2025-01-16 00:58:55 +0300

    Calculator App first commit

--- Sample Calculator/ViewController.swift
+++ Sample Calculator/ViewController.swift
@@ -8,12 +8,44 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var label: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    @IBAction func allClearClicked(_ sender: Any) {
+        label.text = ""
+    }
+    
+    @IBAction func reversePNClicked(_ sender: Any) {
+        
+    }
 
-
+    
+    @IBAction func percentClicked(_ sender: Any) {
+        
+    }
+    
+    @IBAction func digitOneClicked(_ sender: Any) {
+        addChar(value: "1")
+    }
+    
+    @IBAction func digitTwoClicked(_ sender: Any) {
+        addChar(value: "2")
+    }
+    
+    func reversePositiveNegative() {
+        
+    }
+    
+    func addChar(value : String) {
+        label.text = label.text + value
+    }
+    
+    
+    
 }
 
